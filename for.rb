841e57fda100dde4ff8bd3a2698c0dba162a239a
Scott Ungchusri <animoogle504@gmail.com>
2018-06-25 22:06:11 +0000

    Done.

--- for.rb
+++ for.rb
@@ -2,6 +2,14 @@
 def using_for
 	checklist = 1..10
 	#your code here
+	for item in checklist
+	#10.times do 
+	  puts "Wingardium Leviosa"
+  #end
+	end
 end
+
+#was confused at first. Thought that instructions said to  output "Wingardium leviosa"
+#10 times for every item on the checklist resulting in 100 puts.
 
 
